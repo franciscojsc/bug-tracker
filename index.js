4b737df6cd7bbc18d43a91ce3f3d419d66f95bf0
Francisco Chaves <francisco.jschaves@gmail.com>
2019-11-19 16:22:13 -0300

    Conectando à planilha google
    Foi utilizado o Google Apps Script, para fornecer acesso a planilha.

--- index.js
+++ index.js
@@ -2,18 +2,51 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
+const request = require('request')
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve(__dirname, 'views'))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (resquest, response) => {
-    response.render('home')
+app.get('/', (req, res) => {
+    res.render('home')
 })
 
-app.post('/', (resquest, response) => {
-    response.send(resquest.body)
+const url = 'https://script.google.com/macros/s/AKfycbxgwWPbOwDKAsBUUll0d8sacItxX87wKeKiLMf0dPdRiaGd24Q/exec'
+
+app.get('/bug', (req, res) => {
+
+    request
+        .get(`${url}?InitLine=${1}&InitColumn=${1}&numberLine=${10}&numberColumn=${10}`,
+            function (error, response, body) {
+                console.log('error:', error);
+                console.log('statusCode:', response && response.statusCode);
+                console.log('body:', body);
+
+                res.send(body)
+            });
 })
 
-app.listen(3000)
+app.post('/', (req, res) => {
+
+    request.post(url)
+        .form({
+            name: req.body.name,
+            email: req.body.email,
+            issueType: req.body.issueType,
+            howToReproduce: req.body.howToReproduce,
+            expectedOutput: req.body.expectedOutput,
+            receivedOutput: req.body.receivedOutput
+        })
+
+    res.send('Bug reoprtado com sucesso!')
+})
+
+app.listen(3000, (err) => {
+    if (err) {
+        console.log('aconteceu um erro', err)
+    } else {
+        console.log('bugtracker rodando na porta http://localhost:3000')
+    }
+})
