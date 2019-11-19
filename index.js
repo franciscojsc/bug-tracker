525854d616f6990bde7665f71614e4c63027252b
Francisco Chaves <francisco.jschaves@gmail.com>
2019-11-19 17:30:57 -0300

    Utilizando promises nas requisições

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
 const request = require('request')
+const { promisify } = require('util')
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve(__dirname, 'views'))
@@ -15,32 +16,34 @@ app.get('/', (req, res) => {
 
 const url = 'https://script.google.com/macros/s/AKfycbxgwWPbOwDKAsBUUll0d8sacItxX87wKeKiLMf0dPdRiaGd24Q/exec'
 
-app.get('/bug', (req, res) => {
-
-    request
-        .get(`${url}?InitLine=${1}&InitColumn=${1}&numberLine=${10}&numberColumn=${10}`,
-            function (error, response, body) {
-                console.log('error:', error);
-                console.log('statusCode:', response && response.statusCode);
-                console.log('body:', body);
-
-                res.send(body)
-            });
+app.get('/bug', async (req, res) => {
+    try {
+        const { body } = await promisify(request.get)(`${url}?InitLine=${1}&InitColumn=${1}&numberLine=${10}&numberColumn=${10}`)
+        res.send(body)
+    } catch (err) {
+        res.send('Erro ao conectar')
+        console.log(err)
+    }
 })
 
-app.post('/', (req, res) => {
-
-    request.post(url)
-        .form({
-            name: req.body.name,
-            email: req.body.email,
-            issueType: req.body.issueType,
-            howToReproduce: req.body.howToReproduce,
-            expectedOutput: req.body.expectedOutput,
-            receivedOutput: req.body.receivedOutput
+app.post('/', async (req, res) => {
+    try {
+        await promisify(request.post)(url, {
+            form: {
+                name: req.body.name,
+                email: req.body.email,
+                issueType: req.body.issueType,
+                howToReproduce: req.body.howToReproduce,
+                expectedOutput: req.body.expectedOutput,
+                receivedOutput: req.body.receivedOutput
+            }
         })
 
-    res.send('Bug reoprtado com sucesso!')
+        res.send('Bug reoprtado com sucesso!')
+    } catch (err) {
+        res.send('Erro ao conectar')
+        console.log(err)
+    }
 })
 
 app.listen(3000, (err) => {
